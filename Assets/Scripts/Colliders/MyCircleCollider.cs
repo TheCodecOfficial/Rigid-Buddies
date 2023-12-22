b3a4e4c9c166d8bc70624b742c4eac285eff0106
Aniveal <timo.loher@hotmail.com>
2023-12-22 15:05:45 +0100

    Commented Timos classes

--- Assets/Scripts/Colliders/MyCircleCollider.cs
+++ Assets/Scripts/Colliders/MyCircleCollider.cs
@@ -17,12 +17,14 @@ public class MyCircleCollider : MyCollider
         this.myRigidbody.momentOfInertia = 0.5f * myRigidbody.GetMass() * radius * radius;
     }
 
+    //Circle - polygon
     public bool Collides(MyPolygonCollider other)
     {
         Vector2 closestPoint = PolygonUtil.GetClosestPoint(other.GetVerticesArray(), center);
         return Vector2.Distance(closestPoint, center) < radius;
     }
 
+    //Circle - polygon
     public (Vector2, Vector2, Vector2, float) Penetrate(MyPolygonCollider other)
     {
         Vector2 closestPoint = PolygonUtil.GetClosestPoint(other.GetVerticesArray(), center);
@@ -32,11 +34,14 @@ public class MyCircleCollider : MyCollider
         return (closestPoint, otherPoint, normal, depth);
     }
 
+
+    //Circle circle collision
     public bool Collides(MyCircleCollider other)
     {
         return Vector2.Distance(this.center, other.center) < this.radius + other.radius;
     }
 
+    //Circle box collision
     public bool Collides(MyBoxCollider other)
     {
         //In the box collider local transform, SCALE AFFECTED!
@@ -57,29 +62,22 @@ public class MyCircleCollider : MyCollider
 
     }
 
-    //Returns the penetration distance as a vector and the attackPoint
-    //Returns: collisionPoint on this, collisionPoint on other, Normal, depth
-    //Normal is the normal from other outwards
     public (Vector2, Vector2, Vector2, float) Penetrate(MyBoxCollider other)
     {
-
+        //project into boxCollider coordinate space
         Vector2 pos = other.transform.InverseTransformPoint(center);
 
-        //Find closest point on box
+        //Find closest point on box border -> collision point on box
         Vector2 closestPoint = other.FindClosestPoint(pos);
         closestPoint = other.transform.TransformPoint(closestPoint);
 
         Vector2 attackPoint = center + (closestPoint - center).normalized * radius;
         Vector2 direction = (center - attackPoint).normalized;
 
-        //Debug.Log("This: " + this + ", ThisPoint " + attackPoint + ", otherPoint " + closestPoint + ", normal " + direction + ", distance " + Vector2.Distance(attackPoint, closestPoint));
-
         return (attackPoint, closestPoint, direction, Vector2.Distance(attackPoint, closestPoint));
 
     }
 
-    //Returns the penetration distance and position of penetrating point as a vector
-    //Normal is from other to this
     public (Vector2, Vector2, Vector2, float) Penetrate(MyCircleCollider other)
     {
         Vector2 normal = (other.center - center); //From this to other
